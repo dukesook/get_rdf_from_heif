e49fd4a671dabbfcc21ba25ba61b0b1956d6412b
dukesook <devonsookhoo14@gmail.com>
2025-09-10 13:57:05 -0600

    completed

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ uint8_t *data;
 size_t data_size;
 
 int main() {
-  printf("C++14 HEIF RDF Extractor\n");
 
   // Read GIMI File
   const char *filename = "dataset9_capture1_rdf.heif";
@@ -27,19 +26,17 @@ int main() {
   // Get all item IDs
   heif_context_get_list_of_item_IDs(ctx, ids, item_count);
 
-  // Iterate through each item
+  // Find which items are RDF
   for (int i = 0; i < item_count; i++) {
     heif_item_id id = ids[i];
     uint32_t item_type = heif_item_get_item_type(ctx, id);
-    const char *mime_type_c = heif_item_get_mime_item_content_type(ctx, id);
-    string mime_type(mime_type_c ? mime_type_c : "");
-
-    if (item_type == heif_item_type_mime &&
-        mime_type == "text/turtle") {
-
-      heif_item_get_item_data(ctx, id, nullptr, &data, &data_size);
-      const char *rdf = reinterpret_cast<const char *>(data);
-      cout << "RDF Data: " << rdf << endl;
+    if (item_type == heif_item_type_mime) {
+      string mime_type = heif_item_get_mime_item_content_type(ctx, id);
+      if (mime_type == "text/turtle") {
+        heif_item_get_item_data(ctx, id, nullptr, &data, &data_size);
+        const char *rdf = reinterpret_cast<const char *>(data);
+        cout << "RDF Data: " << rdf << endl;
+      }
     }
   }
 
